9005e93cd074889371979a71f45179c0ed51ce7f
joaovictormendes <31655948+joaovictormendes@users.noreply.github.com>
2017-12-11 21:38:02 -0200

    fim de aula

--- aula20170906/ale1.c
+++ aula20170906/ale1.c
@@ -1,9 +1,7 @@
-#include <iostream>
 #include <stdlib.h>
-
 #include <time.h>
 #include <stdio.h>
-using namespace std;
+#include <conio.h>
 
 void main ();
 {
@@ -12,5 +10,6 @@ void main ();
 	var = rand()%100;	
 	var2 = 101 + rand()%294;
 	printf ("Primeiro numero sorteado: %d! Segundo numero sorteado: %d!\n", var, var2);
+	getche ();
 	return 0;
 }
